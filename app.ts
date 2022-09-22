be5b1b1be695fb7eb0e701c09121a7f680faa552
ridvanbirgul <ridvanbirgul@hotmail.com>
2022-09-22 06:22:50 +0300

    cors changed

--- app.ts
+++ app.ts
@@ -42,7 +42,13 @@ app.use(expressWinston.logger(loggerOptions));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors());
+//app.use(cors());
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    })
+);
 app.use(ignoreFavicon);
 app.use(logger('dev'));
 app.use(express.json());
