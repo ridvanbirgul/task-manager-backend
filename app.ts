c06eb093774fd0807adc81311b5884f59033b605
ridvanbirgul <ridvanbirgul@hotmail.com>
2022-09-22 06:18:26 +0300

    cors putted to top

--- app.ts
+++ app.ts
@@ -42,13 +42,14 @@ app.use(expressWinston.logger(loggerOptions));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(ignoreFavicon);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
 let optionsOfLimiter: Partial<Options> = {
     max: 100,
     windowMs: 15 * 60 * 1000,
